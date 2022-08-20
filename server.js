412d8f10f44ececbbec9e3b4ded59d010cd57c1f
aditya12gusain <adisandi3112002@gmail.com>
2022-08-20 21:15:39 +0530

    .

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 const http = require("http");
+const port = 8081;
 
-const port = 8081; // local port no
+const toDoList = ["Complete Node Byte", "Play Cricket"];
 
 http
   .createServer((req, res) => {
-    // callback function
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Hello, this is from my server</h1>");
+    const { method, url } = req;
+
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(toDoList.toString());
+      } else if (method === "POST") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            console.log("data: ", body);
+          });
+      } else {
+        res.writeHead(501);
+      }
+    } else {
+      res.writeHead(404);
+    }
+
     res.end();
   })
   .listen(port, () => {
-    // callback function
     console.log(`Nodejs server started on port ${port}`);
   });
 
