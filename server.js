5553735f1cea4b092f1c530db2f78ffbc87bd978
aditya12gusain <adisandi3112002@gmail.com>
2022-08-22 21:40:11 +0530

    added new routes

--- server.js
+++ server.js
@@ -22,7 +22,38 @@ http
           })
           .on("end", () => {
             body = JSON.parse(body);
-            console.log("data: ", body);
+            let newToDo = toDoList;
+            newToDo.push(body.item);
+            console.log(newToDo);
+            res.writeHead(201);
+          });
+      } else if (method === "DELETE") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let deleteThis = body.item;
+
+            // for (let i = 0; i < toDoList.length; i++) {
+            //   if (toDoList[i] === deleteThis) {
+            //     toDoList.splice(i, 1);
+            //     break;
+            //   }
+            // }
+
+            toDoList.find((element, index) => {
+              if (element === deleteThis) {
+                toDoList.splice(index, 1);
+              }
+            });
+
+            res.writeHead(204);
           });
       } else {
         res.writeHead(501);
